f9eeb9e5e98bc56c5e8fec85033a238ef74ade19
shubhamholkar007 <umareshreyas@gmail.com>
2020-02-07 16:00:59 +0530

    updated

--- js/10_DELETE.js
+++ js/10_DELETE.js
@@ -1,24 +1,15 @@
-data ={
-"name": "morpheus",
-"job": "zion resident"
-}
-
-fetch('https://reqres.in/api/users/2',{
-method: 'DELETE',
-headers: new Headers ({
-"Content-type" : "application/json"
-}), 
-body: JSON.stringify(data)
-
+fetch("https://reqres.in/api/users/2", {
+  method: "DELETE"
 })
-.then(function(response){
-if(response.status !== 204)
-console.log("Looks like there was a problem. Status Code:"+response.status)
-else
-response.json().then(data =>
-console.log(data)
-)}
-
-).catch(function(err) {
-console.log('Fetch Error :-S', err);
-});
+  .then(function(response) {
+    if (response.status !== 204)
+      console.log(
+        "Looks like there was a problem. Status Code:" + response.status
+      );
+    else {
+      console.log(response);
+    }
+  })
+  .catch(function(err) {
+    console.log("Fetch Error :-S", err);
+  });
--- js/15_DELAYED_RESPONSE.js
+++ js/15_DELAYED_RESPONSE.js
@@ -1,15 +1,12 @@
-fetch('https://reqres.in/api/users?delay=3')
-.then(function(response){
-if(response.status !== 200)
-{
-console.log("Looks like there was a problem. Status Code:"+response.status)
-response.json().then(data => 
-console.log(data)
-)}
-else
-response.json().then(data =>
-console.log(data)
-)}
-).catch(function(err) {
-console.log('Fetch Error :', err);
-});
+fetch("https://reqres.in/api/users?delay=3")
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log(
+        "Looks like there was a problem. Status Code:" + response.status
+      );
+      response.json().then(data => console.log(data));
+    } else response.json().then(data => console.log(data));
+  })
+  .catch(function(err) {
+    console.log("Fetch Error :", err);
+  });
